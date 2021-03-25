05fd4fa02e97c2a8cdff7d75c17c0ccb28a29308
Manil MEZZI <manil.mezzi.dz@viacesi.fr>
2021-03-25 13:51:53 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -67,5 +67,4 @@ Route::get('/nav', function () {
 });
 
 
-Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'index'])->name('register');
-Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
+Auth::route();
